43144f7e857736b7cf1221374a37c585f2e4e13f
guilhermegattimarinho <ggattimarinho@gmail.com>
2024-02-02 22:04:11 -0300

    ✨ feat: implementa translate_controller e lógica para permitir  a tradução

---
+++ src/controllers/translate_controller.py
@@ -0,0 +1,53 @@
+from flask import request, render_template, Blueprint
+from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
+
+
+bp = Blueprint("translate_controller", __name__)
+
+
+@bp.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+        # history
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+        print(LanguageModel.list_dicts(), "languages")
+        return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate="O que deseja traduzir?",
+        translate_from="pt",
+        translate_to="en",
+        translated="What do you want to translate?",
+    )
+
+
+@bp.route("/reverse", methods=["POST"])
+def reverse():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
